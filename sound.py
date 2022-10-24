7d6f33b9344ed294192f1d77349102efc415d8cc
gerardlagos <gerardlagos01@gmail.com>
2022-10-23 22:40:34 -0700

    AUDIO plays but needs to be fixed (constantly playing)

--- sound.py
+++ sound.py
@@ -8,7 +8,9 @@ class Sound:
         pg.mixer.music.load(bg_music)
         pg.mixer.music.set_volume(0.1)
         gameover_sound = pg.mixer.Sound('sounds/pacman_death.wav')
-        self.sounds = {'gameover': gameover_sound}
+        eatfruit_sound = pg.mixer.Sound('sounds/pacman_eatfruit.wav')
+        eatdot_sound = pg.mixer.Sound('sounds/wakawaka.wav')
+        self.sounds = {'gameover': gameover_sound, 'eatfruit':eatfruit_sound, 'eatdot': eatdot_sound}
 
     def play_bg(self):
         pg.mixer.music.play(-1, 0.0)
@@ -16,6 +18,11 @@ class Sound:
     def stop_bg(self):
         pg.mixer.music.stop()
 
+    def eat_fruit(self):
+        pg.mixer.Sound.play(self.sound['eatfruit'])
+
+    def eat_dot(self):
+        pg.mixer.Sound.play(self.sound['eatdot'])
 
     def gameover(self):
         self.stop_bg()
